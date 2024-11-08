085dfff9aad2ad1051dc52830083e12f051fad05
dmatijani <dmatijani21@foi.hr>
2024-11-08 22:09:51 +0100

    generiranje ključeva gotovo

---
+++ WebApp/Components/PageComponent.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace WebApp.Components;
+
+public partial class PageComponent
+{
+    [Parameter]
+    public string Title { get; set; } = "Naslov";
+
+    [Parameter]
+    public RenderFragment ChildContent { get; set; }
+}
---
+++ WebApp/Pages/Keys.razor.cs
@@ -0,0 +1,44 @@
+﻿using Application.Services;
+using Microsoft.AspNetCore.Components;
+using WebApp.Services;
+
+namespace WebApp.Pages;
+
+public partial class Keys
+{
+    [Inject]
+    private IAesKeyGenerator AesKeyGenerator { get; set; }
+
+    [Inject]
+    private IRsaKeyGenerator RsaKeyGenerator { get; set; }
+
+    [Inject]
+    private IFileService FileService { get; set; }
+
+    private bool _instantDownload = true;
+
+    private string _aesKey = string.Empty;
+    private string _rsaPublicKey = string.Empty;
+    private string _rsaPrivateKey = string.Empty;
+
+    private async Task GenerateAesKey()
+    {
+        _aesKey = AesKeyGenerator.GenerateKey();
+        if (_instantDownload)
+        {
+			await FileService.DownloadText("tajni_kljuc.txt", _aesKey);
+		}
+    }
+    
+    private async Task GenerateRsaKeys()
+    {
+        var _rsaKeys = RsaKeyGenerator.GenerateKeyPair();
+        _rsaPublicKey = _rsaKeys.PublicKey;
+        _rsaPrivateKey = _rsaKeys.PrivateKey;
+        if (_instantDownload)
+        {
+			await FileService.DownloadText("javni_kljuc.txt", _rsaPublicKey);
+			await FileService.DownloadText("privatni_kljuc.txt", _rsaPrivateKey);
+		}
+    }
+}
