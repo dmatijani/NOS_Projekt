29f85980e244d85e91a967425241ca873afea18a
dmatijani <dmatijani21@foi.hr>
2024-11-08 22:45:56 +0100

    Mrčku refactor

--- WebApp/Pages/Keys.razor.cs
+++ WebApp/Pages/Keys.razor.cs
@@ -17,24 +17,48 @@ public partial class Keys
     [Inject]
     private Global Global { get; set; }
 
-    private async Task GenerateAesKey()
+	private bool _instantDownload
+	{
+		get => Global.InstantDownload;
+		set => Global.InstantDownload = value;
+	}
+
+	private string _aesKey
+	{
+		get => Global.AesKey;
+		set => Global.AesKey = value;
+	}
+
+	private string _rsaPublicKey
+	{
+		get => Global.RsaPublicKey;
+		set => Global.RsaPublicKey = value;
+	}
+
+	private string _rsaPrivateKey
+	{
+		get => Global.RsaPrivateKey;
+		set => Global.RsaPrivateKey = value;
+	}
+
+	private async Task GenerateAesKey()
     {
-        Global.AesKey = AesKeyGenerator.GenerateKey();
-        if (Global.InstantDownload)
+        _aesKey = AesKeyGenerator.GenerateKey();
+        if (_instantDownload)
         {
-			await FileService.DownloadText("tajni_kljuc.txt", Global.AesKey);
+			await FileService.DownloadText("tajni_kljuc.txt", _aesKey);
 		}
     }
     
     private async Task GenerateRsaKeys()
     {
         var _rsaKeys = RsaKeyGenerator.GenerateKeyPair();
-        Global.RsaPublicKey = _rsaKeys.PublicKey;
-        Global.RsaPrivateKey = _rsaKeys.PrivateKey;
-        if (Global.InstantDownload)
+        _rsaPublicKey = _rsaKeys.PublicKey;
+        _rsaPrivateKey = _rsaKeys.PrivateKey;
+        if (_instantDownload)
         {
-			await FileService.DownloadText("javni_kljuc.txt", Global.RsaPublicKey);
-			await FileService.DownloadText("privatni_kljuc.txt", Global.RsaPrivateKey);
+			await FileService.DownloadText("javni_kljuc.txt", _rsaPublicKey);
+			await FileService.DownloadText("privatni_kljuc.txt", _rsaPrivateKey);
 		}
     }
 }
